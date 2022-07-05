dc573396b6e1f2e95d34b6233ea34e163d474e4b
Jamie Lear <jamie@barcombe.net>
2022-07-05 14:58:52 +0100

    Updated documentation for bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -188,6 +188,7 @@ def test():
     df['day_of_week'] = df['Start Time'].dt.day_name()
     print(df['day_of_week'])    
 
+#Perform data query
 def main():
     while True:
         city, month, day = get_filters()
